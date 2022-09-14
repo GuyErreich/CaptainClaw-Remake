921281a969bfc725a7f54a5e316696b8415ab497
GuyErreich <guyerreich@gmail.com>
2022-09-15 00:21:06 +0300

    Added Physics based jump triggering

--- Assets/MainProject/Scripts/Player/Modes/JumpingMode.cs
+++ Assets/MainProject/Scripts/Player/Modes/JumpingMode.cs
@@ -6,11 +6,12 @@ namespace CaptainClaw.Scripts.Player
     [RequireComponent(typeof(MovementHandler))]
     public class JumpingMode : PlayerState {
         [Header("Stats")]
+        [SerializeField] private bool usePhysics = false;
+        [SerializeField] private float jumpForce = 7f;
+        [SerializeField] private float jumpGracePeriod = 0.2f;
         [SerializeField] private float speed = 2f;
         [SerializeField] private float rotationSpeed = 2f;
         [SerializeField, Range(1f, 5f)] private float sprintMultiplier = 1.2f;
-        [SerializeField] private float jumpForce = 7f;
-        [SerializeField] private float jumpGracePeriod = 0.2f;
 
         private DetectCollision detectCollider;
 
@@ -20,12 +21,22 @@ namespace CaptainClaw.Scripts.Player
 
         public override IEnumerator On() {
             PlayerStates nextState;
+    
+            var direction = Vector3.zero;
+            var finalSpeed = 0f;
+
+            if (this.usePhysics) {
+                direction = this.GetComponent<CharacterController>().velocity;
+                finalSpeed = 1f;
+            }
 
             while (true)
             {
-                var direction = (this.transform.right * InputReceiver.Movement.x) + (this.transform.forward * InputReceiver.Movement.y);
-                var finalSpeed = (InputReceiver.RunPressed ? this.sprintMultiplier : 1f);
-                finalSpeed *= this.speed;
+                if (!this.usePhysics) {
+                    direction = (this.transform.right * InputReceiver.Movement.x) + (this.transform.forward * InputReceiver.Movement.y);
+                    finalSpeed = (InputReceiver.RunPressed ? this.sprintMultiplier : 1f);
+                    finalSpeed *= this.speed;
+                }
 
                 MovementHandler.Jump(this.jumpForce, this.jumpGracePeriod);
                 MovementHandler.Move(direction, finalSpeed);
